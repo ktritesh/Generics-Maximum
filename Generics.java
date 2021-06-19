8b20838af3c3094fe90cf469e9212b3d815b0c63
ktritsh7 <ktriesh7@gmail.com>
2021-06-19 15:07:20 +0530

    Welcome to Generics Test cases Problem

---
+++ Generics.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.generics;
+
+public class Generics {
+
+    public static void main(String[] args) {
+         System.out.println(Welcome to Generics Test cases Problem);
+    }
+}
