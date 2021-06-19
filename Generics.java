fa67cab92cd3c5fdd34a33f926dc7254c82a3341
ktritsh7 <ktriesh7@gmail.com>
2021-06-19 19:04:43 +0530

    Refactor Generic class to take three variables

--- Generics.java
+++ Generics.java
@@ -6,25 +6,13 @@ public class Generics {
         Integer [] int1 = {1,2,3};
         Double [] dub1 = {1.1, 1.2, 2.2, 3.3, 4.4};
         String [] str1 = {"Apple", "Peach", "Banana", "Orange", "Mango"};
-        Generics.printInt(int1);
-        Generics.printDouble(dub1);
-        Generics.printStr(str1);
+        Generics.printArray(int1);
+        Generics.printArray(dub1);
+        Generics.printArray(str1);
     }
 
-    public static void printInt(Integer [] integers){
-        for (int i : integers){
-            System.out.println(i);
-        }
-    }
-
-    public static void printDouble(Double [] doubles){
-        for (double i : doubles){
-            System.out.println(i);
-        }
-    }
-
-    public static void printStr(String [] strings){
-        for (String i : strings){
+    public static <E> void printArray(E[] elements){
+        for (E i : elements){
             System.out.println(i);
         }
     }
